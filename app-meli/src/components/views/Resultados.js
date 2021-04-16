cdd8b28a08de63bd200445d85403719a062684a4
rodrigortigosa <rodrigortigosa@gmail.com>
2021-04-16 16:46:12 -0300

    Corrijo flexbox

--- app-meli/src/components/views/Resultados.js
+++ app-meli/src/components/views/Resultados.js
@@ -44,13 +44,13 @@ export const Resultados = () => {
         </div> 
         :
         <div className="list-products">
-          { 
-            <ul>
-              {products.map(product => (
-                <li key={product.id}>{product.title}</li>
-              ))}
-            </ul>
-          }
+            { 
+              products.map(product => (
+                <div>
+                  <p key={product.id}>{product.title}</p>
+                </div>
+              ))
+            }
         </div>
       }
     </div>
