2a4d9cfb46a1757923bd6484e5ab153798ca4c5e
rodrigortigosa <rodrigortigosa@gmail.com>
2021-04-22 17:09:35 -0300

    Agrego anchor para los productos y corrijo path

--- app-meli/src/components/views/Productos.js
+++ app-meli/src/components/views/Productos.js
@@ -1,7 +1,7 @@
 import React from "react";
 import free_shipping from '../../images/shipping.png';
 import "../../styles/Productos.css";
-
+import { DETALLE } from '../../config/router/paths';
 const separarMiles = (num) => {
   return num.toLocaleString('de-DE');
 }
@@ -12,29 +12,31 @@ export const Productos = ({productos}) => {
       {
         productos.map(product => {
           return (
-            <div key={product.id} className="product">
-              <div className="product-img">
-                <img src={product.thumbnail} alt="Imagen del producto" width="180px"></img>
-              </div>
-              <div className="product-info">
-                <div className="product-price">
-                  {
-                    product.shipping.free_shipping ? 
-                    <span>
-                      $ {separarMiles(product.price)}
-                      <img className="img-free-shipping" src={free_shipping} alt="Envio gratis"></img>
-                    </span>
-                    :
-                    <span>$ {separarMiles(product.price)}</span>
-                  }
+            <a key={product.id} href={`${DETALLE}/${product.id}`}>
+              <div className="product">
+                <div className="product-img">
+                  <img src={product.thumbnail} alt="Imagen del producto" width="180px"></img>
                 </div>
-                <div className="product-location">
-                  <p>{product.address.state_name}</p>
+                <div className="product-info">
+                  <div className="product-price">
+                    {
+                      product.shipping.free_shipping ? 
+                      <span>
+                        $ {separarMiles(product.price)}
+                        <img className="img-free-shipping" src={free_shipping} alt="Envio gratis"></img>
+                      </span>
+                      :
+                      <span>$ {separarMiles(product.price)}</span>
+                    }
+                  </div>
+                  <div className="product-location">
+                    <p>{product.address.state_name}</p>
+                  </div>
+                  <h2>{product.title}</h2>
+                  <p>Completo Unico!</p>
                 </div>
-                <h2>{product.title}</h2>
-                <p>Completo Unico!</p>
               </div>
-            </div>
+            </a>
           )
         })
       }
