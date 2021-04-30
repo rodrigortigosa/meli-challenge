5d00dc7e2829896098276f3398b93e5a10b01e90
rodrigortigosa <rodrigortigosa@gmail.com>
2021-04-30 16:16:22 -0300

    Revert "Separo los miles en precio del detalle"
    This reverts commit 091f5d894b0d6bb397bc826a881ee3b7a47cd76e.

--- app-meli/src/components/views/Detalle.js
+++ app-meli/src/components/views/Detalle.js
@@ -7,10 +7,6 @@ import '../../styles/Resultados.css';
 import '../../styles/Detalle.css';
 //import { Categorias } from "./Categorias";
 
-const separarMiles = (num) => {
-  return num.toLocaleString('de-DE');
-}
-
 export const Detalle = () => {
   const id = useParams().id;
 
@@ -58,7 +54,7 @@ export const Detalle = () => {
                     {`- ${product.sold_quantity} vendidos`}</span>
                 </div>
                 <h1>{product.title}</h1>
-                <p>$ {separarMiles(product.price)}</p>
+                <p>$ {product.price}</p>
                 <div>
                   <a className="btn btn-primary" role="button" href={product.permalink} target="_blank" rel="noreferrer">
                     Comprar
